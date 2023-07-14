b76fbd42d1b7744911123ff84bcd386e3d3d8e5e
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-07-14 13:24:22 -0400

    Task 1: Created Solution and added a MVC web application

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Assignment_WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
