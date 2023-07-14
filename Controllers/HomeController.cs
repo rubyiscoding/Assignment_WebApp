8c34f916c618ada736e452ab7d65e1ac08fb785d
RubyIsCoding <ruby.chaulagain@yahoo.com>
2023-07-14 13:31:39 -0400

    Task 2: Added new action method Intro and added respective view.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,4 +28,11 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
+    [HttpGet]
+    public IActionResult Intro ()
+    {
+        return View();
+    }
+
 }
